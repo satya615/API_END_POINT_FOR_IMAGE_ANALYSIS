f42faf61df9ac287b27b054367739e3f10ce7399
satya615 <vsatyanarayana@gmail.com>
2025-02-11 09:34:14 +0530

    first commit

--- sentiment_app/settings.py
+++ sentiment_app/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+PYTHON_VERSION = os.getenv('PYTHON_VERSION', '3.10.4')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
